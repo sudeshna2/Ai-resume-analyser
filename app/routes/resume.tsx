1e0554a14fb9d1599a18a19b0c816c5812e7e807
sudeshna2 <sudeshna23062003@gmail.com>
2025-08-16 13:21:02 +0530

    Resume Feedback Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -6,7 +6,7 @@ import ATS from "~/components/ATS";
 import Details from "~/components/details";
 
 export const meta = () => ([
-    { title: 'Resumind | Review ' },
+    { title: 'UrResume | Review ' },
     { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
@@ -76,7 +76,7 @@ const Resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
-                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <ATS score ={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
                         </div>
                     ) : (
